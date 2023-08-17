c79e41b7ea9d3166f288c2e091aec0c82fe0e68d
Kay <kehiiiiya@gmail.com>
2023-08-17 21:48:36 +0800

    feat: add logger

--- core/resolver.go
+++ core/resolver.go
@@ -8,11 +8,12 @@ import (
 	"net"
 	"strings"
 
+	"github.com/kehiy/dns-server/logger"
 	"golang.org/x/net/dns/dnsmessage"
 )
 
-const ROOTSERVERS = `198.41.0.4,199.9.14.201,192.33.4.12,199.7.91.13,192.203.230.10,192.5.5.241,
-	192.112.36.4,198.97.190.53`
+const ROOTSERVERS = `198.41.0.4,199.9.14.201,192.33.4.12,199.7.91.13,
+					 192.203.230.10,192.5.5.241,192.112.36.4,198.97.190.53`
 
 func HandlePacket(pc net.PacketConn, addr net.Addr, buf []byte) {
 	//* send incoming packets to handlePacket function
@@ -122,7 +123,7 @@ func dnsQuery(servers []net.IP, question dnsmessage.Question) (*dnsmessage.Messa
 							Type: dnsmessage.TypeA, Class: dnsmessage.ClassINET,
 						})
 					if err != nil {
-						fmt.Printf("warning: lookup of nameserver %s failed: %err\n", nameserver, err)
+						logger.Warn("lookup of nameserver failed", "nameserver", nameserver, "error", err)
 					} else {
 						newResolverServersFound = true
 						for _, answer := range response.Answers {
---
+++ logger/logger_test.go
@@ -0,0 +1,42 @@
+package logger
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/rs/zerolog/log"
+	"github.com/stretchr/testify/assert"
+)
+
+type Foo struct{}
+
+func (f Foo) String() string {
+	return "foo"
+}
+
+func TestLogger(t *testing.T) {
+	var buf bytes.Buffer
+	log.Logger = log.Output(&buf)
+
+	Trace("a")
+	Info("b", nil)
+	Info("b", "a", nil)
+	Info("c", "b", []byte{1, 2, 3})
+	Warn("d", "x")
+	Error("e", "y", Foo{})
+
+	out := buf.String()
+
+	fmt.Println(out)
+	assert.Contains(t, out, "foo")
+	assert.Contains(t, out, "010203")
+	assert.Contains(t, out, "!INVALID-KEY!")
+	assert.Contains(t, out, "!MISSING-VALUE!")
+	assert.Contains(t, out, "null")
+	assert.NotContains(t, out, "trace")
+	assert.NotContains(t, out, "debug")
+	assert.Contains(t, out, "info")
+	assert.Contains(t, out, "warn")
+	assert.Contains(t, out, "error")
+}
