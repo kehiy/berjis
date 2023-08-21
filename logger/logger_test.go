c17cf495327be99906cc3882e1815781de52dbad
Kay <kehiiiiya@gmail.com>
2023-08-22 01:32:44 +0330

    chore: add more linter (#14)

--- logger/logger_test.go
+++ logger/logger_test.go
@@ -1,10 +1,11 @@
-package logger
+package logger_test
 
 import (
 	"bytes"
 	"fmt"
 	"testing"
 
+	"github.com/kehiy/berjis/logger"
 	"github.com/rs/zerolog/log"
 	"github.com/stretchr/testify/assert"
 )
@@ -19,12 +20,12 @@ func TestLogger(t *testing.T) {
 	var buf bytes.Buffer
 	log.Logger = log.Output(&buf)
 
-	Trace("a", "ok", "!ok")
-	Info("b", nil)
-	Info("b", "a", nil)
-	Info("c", "b", []byte{1, 2, 3})
-	Warn("d", "x")
-	Error("e", "y", Foo{})
+	logger.Trace("a", "ok", "!ok")
+	logger.Info("b", nil)
+	logger.Info("b", "a", nil)
+	logger.Info("c", "b", []byte{1, 2, 3})
+	logger.Warn("d", "x")
+	logger.Error("e", "y", Foo{})
 
 	out := buf.String()
 
