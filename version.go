5209be350737584ee352b7bcf9ee7d3342950d7d
Kay <kehiiiiya@gmail.com>
2023-08-22 06:10:24 +0800

    feat:  file

---
+++ version.go
@@ -0,0 +1,33 @@
+package berjis
+
+import (
+	"fmt"
+)
+
+// These constants follow the semantic versioning 2.0.0 spec (http://semver.org/)
+const (
+	major uint   = 0
+	minor uint   = 1
+	patch uint   = 0
+	meta  string = "beta"
+)
+
+var build string
+
+func Agent() string {
+	return fmt.Sprintf("berjis/%s", Version())
+}
+
+func Version() string {
+	version := fmt.Sprintf("%d.%d.%d", major, minor, patch)
+
+	if meta != "" {
+		version = fmt.Sprintf("%s-%s", version, meta)
+	}
+
+	if build != "" {
+		version = fmt.Sprintf("%s+%s", version, build)
+	}
+
+	return version
+}
